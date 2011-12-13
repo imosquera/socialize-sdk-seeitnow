92131c21517aabcafacba464a059e8827ca1f397
Isaac Mosquera <imosquer@gmail.com>
2011-12-13 11:18:12 -0800

    Initial Commit

---
+++ Socialize/SocializeAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SocializeAppDelegate.h
+//  Socialize
+//
+//  Created by Isaac Mosquera on 12/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SocializeViewController;
+
+@interface SocializeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SocializeViewController *viewController;
+
+@end
---
+++ Socialize/SocializeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SocializeViewController.h
+//  Socialize
+//
+//  Created by Isaac Mosquera on 12/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocializeViewController : UIViewController
+
+@end
